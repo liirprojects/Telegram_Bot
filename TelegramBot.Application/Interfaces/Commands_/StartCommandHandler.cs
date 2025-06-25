a6d91d78ebe86456aeb04321b2dd989cf18e906d
valeriiia2003 <leraspasibukhova@ukr.net>
2025-06-25 13:29:46 +0100

    StartCommandHandler has been created as a new command for a IEnumerable<ICommandHandler> list

---
+++ TelegramBot.Application/Interfaces/Commands_/StartCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace TelegramBot.Application.Interfaces.Commands;
+
+public class StartCommandHandler : CommandHandler
+{
+    public override string Command => "/start";
+
+    public StartCommandHandler(IMessageSender sender) : base(sender) { }
+
+    public override Task HandleAsync(long chatId, string message, CancellationToken cancellationToken)
+    {
+        return _messageSender.SendTextAsync(chatId, "👋 Hello! I'm your bot.", cancellationToken);
+    }
+}
