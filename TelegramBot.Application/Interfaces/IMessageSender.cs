eace6ea875c0b68610a065b9b517d098440d798f
valeriiia2003 <leraspasibukhova@ukr.net>
2025-06-25 10:56:53 +0100

    New logic for sending messages to the user has been added (IMessageSender,TelegramMessageSender)

---
+++ TelegramBot.Application/Interfaces/IMessageSender.cs
@@ -0,0 +1,13 @@
+﻿namespace TelegramBot.Application.Interfaces;
+
+public interface IMessageSender
+{
+    /// <summary>
+    /// Sends a message to a specific chat.
+    /// </summary>
+    /// <param name="chatId">The ID of the chat to send the message to.</param>
+    /// <param name="message">The message to send.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task SendTextAsync(long chatId, string message, CancellationToken cancellationToken);
+}
---
+++ TelegramBot.Infrastructure/Services/TelegramMessageSender.cs
@@ -0,0 +1,23 @@
+﻿using Telegram.Bot.Types.Enums;
+using Telegram.Bot;
+using TelegramBot.Application.Interfaces;
+
+namespace TelegramBot.Infrastructure.Services;
+public class TelegramMessageSender : IMessageSender
+{
+    private readonly ITelegramBotClient _botClient;
+
+    public TelegramMessageSender(ITelegramBotClient botClient)
+    {
+        _botClient = botClient;
+    }
+
+    public Task SendTextAsync(long chatId, string text, CancellationToken token)
+    {
+        return _botClient.SendMessage(
+            chatId,
+            text,
+            parseMode: ParseMode.Markdown,
+            cancellationToken: token);
+    }
+}
