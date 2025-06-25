3ea8bac93507656ee827e4990709ff754604134e
valeriiia2003 <leraspasibukhova@ukr.net>
2025-06-25 09:44:54 +0100

    Add ICommandHandler interface and abstract base CommandHandler

---
+++ TelegramBot.Application/Interfaces/Commands_/CommandHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace TelegramBot.Application.Interfaces.Commands;
+
+public abstract class CommandHandler : ICommandHandler
+{
+    public abstract string Command { get; }
+
+    public abstract Task HandleAsync(long chatId, string message, CancellationToken cancellationToken);
+
+    protected readonly IMessageService _messageService;
+
+    protected CommandHandler(IMessageService messageService)
+    {
+        _messageService = messageService;
+    }
+}
---
+++ TelegramBot.Application/Interfaces/ICommandHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace TelegramBot.Application.Interfaces;
+
+
+public interface ICommandHandler
+{
+    string Command { get; }
+    Task HandleAsync(long chatId, string message, CancellationToken cancellationToken);
+}
