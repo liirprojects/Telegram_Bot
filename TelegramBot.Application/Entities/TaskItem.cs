44102c8ae7f662fed24bbac534d6bb83b885334c
valeriiia2003 <leraspasibukhova@ukr.net>
2025-07-02 16:51:25 +0100

    Add entity to represent users task

---
+++ TelegramBot.Application/Entities/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿namespace TelegramBot.Application.Entities;
+
+public class TaskItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid(); // Уникальный ID
+
+    public long ChatId { get; set; } // ID пользователя Telegram
+
+    public string Title { get; set; } = null!; // Название задачи
+
+    public string? Description { get; set; } // Описание (опционально)
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? DueDate { get; set; } // Дедлайн
+
+    public bool IsCompleted { get; set; } = false; // Статус задачи
+}
