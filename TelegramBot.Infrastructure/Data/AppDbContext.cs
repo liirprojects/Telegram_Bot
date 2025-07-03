709b0e7c0a21968175a84d9aa5b13febe76be0f8
valeriiia2003 <leraspasibukhova@ukr.net>
2025-07-03 18:16:49 +0100

    Add database management/connection options

--- TelegramBot.Infrastructure/Data/AppDbContext.cs
+++ TelegramBot.Infrastructure/Data/AppDbContext.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using TelegramBot.Application.Entities;
+namespace TelegramBot.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TaskItem> Tasks { get; set; } = null!;
+}
